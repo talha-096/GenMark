286ed532ea59fd39bf9cc1d36a9e1e253e068b47
Your Name <your-email@example.com>
2026-04-11 23:10:51 +0500

    Fix Frontend CI: exclude test/setup files from ESLint (lint step was failing on as-any mocks)

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/setupTests.ts', 'src/**/*.test.ts', 'src/**/*.test.tsx', 'src/**/*.spec.ts', 'src/**/*.spec.tsx'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
